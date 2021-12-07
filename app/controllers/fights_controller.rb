68081a37f97f309c6bd699860a34f469c87514b6
John78468 <philantrope1@outlook.com>
2021-12-08 00:37:22 +0100

    show,new fight, no congif routes edit,delete,update

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -1,9 +1,42 @@
 class FightsController < ApplicationController
   def index
-    
-  end
+    @fights = Figth.all
+  end 
 
   def show
-    
+    @fights = Figth.find(params[:id])
+  end
+
+  def new
+    @fights = Figth.new
+  end
+
+  def create
+    @fights = Figth.new(product_params)
+    if @fight.save
+      puts "combat sauvegarder"      
+    else
+      puts "recommencez"
+      render :new
+    end
+  end
+
+  def edit
+    @fight = Figth.find(params[:id])
+  end
+
+  def destroy 
+    @fight = Figth.find(params[:id])
+    if @fight.delete
+      puts "archive du combat detruite"
+      redirect_to root_path
+    else
+      puts "archive du combat non detruite"
+      render :destroy
+    end
+  end
+
+  def product_params
+    params.require(:fight).permit
   end
 end
